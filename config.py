d9198752b9421452a44b53be016b4d54e641cca1
jeetc2003 <127688594+jeetc2003@users.noreply.github.com>
2026-04-10 11:51:42 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+# Weather API configuration
+API_KEY = "12b3ab8a9be94316aa243910260704"
+API_URL = "http://api.weatherapi.com/v1/history.json"
+
+# CSV file location
+CSV_FILE_PATH = "docs/weather_data.csv"
+ARCHIVE_CSV_FILE_PATH = "archive/historical_data.csv"
+
+# DB configuration
+DB_SERVER = "dnaonboadingcentinsql01.database.windows.net"
+DB_DATABASE = "DNAOnboarding-CentIN-SQLDB01"
